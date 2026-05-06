b16b46e1f133c664595c5a4896ad574c8bcb67b0
Pritam Dhara <mtforstartups@gmail.com>
2026-05-06 14:01:45 +0530

    Added Fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,11 @@
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
 
+	//Fonts
+	import '@fontsource-variable/dm-sans/wght.css';
+	import '@fontsource-variable/syne/wght.css';
+	import '@fontsource-variable/playfair-display/wght-italic.css';
+
 	let { children } = $props();
 </script>
 
